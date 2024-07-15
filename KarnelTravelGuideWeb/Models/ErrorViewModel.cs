e4e6cdc9ddfdabff993eb03f63de6872afe8c337
PNA <pna120104@gmail.com>
2024-07-15 09:07:37 +0700

    15/07/2024

---
+++ KarnelTravelGuideWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KarnelTravelGuideWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
