c7d95786c455e218ebc20a3d7dbe9ba6ac9dda7c
ADMIN-PC\hi <tranquanglinh619@gmail.com>
2024-07-23 22:35:37 +0700

    pushing backend code

---
+++ KarnelTravelGuideWeb/ViewModels/ResortVM.cs
@@ -0,0 +1,21 @@
+﻿namespace KarnelTravelGuideWeb.ViewModels
+{
+    public class ResortVM
+    {
+        public int ResortId { get; set; }
+
+        public string? Img { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int QualityRating { get; set; }
+
+        public bool? Availability { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
