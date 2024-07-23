91ff07702bf8ea8cf380471e15298fb961aae0fe
ADMIN-PC\hi <tranquanglinh619@gmail.com>
2024-07-23 23:16:10 +0700

    no message

---
+++ KarnelTravelGuideWeb/Models/Transportation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KarnelTravelGuideWeb.Models;
+
+public partial class Transportation
+{
+    public int TransportId { get; set; }
+
+    public string? Img { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public int? CityId { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool? Availability { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual City? City { get; set; }
+}
---
+++ KarnelTravelGuideWeb/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// wwwroot/js/site.js
+
+function openSearch() {
+    var div = document.getElementById("search-container");
+    if (div.style.display === "none" || div.style.display === "") {
+        div.style.display = "flex";
+        div.style.transition = "0.4s ease";
+    }
+}
+function closeSearch() {
+    var div = document.getElementById("search-container");
+    if (div.style.display === "flex" || div.style.display === "") {
+        div.style.display = "none";
+        div.style.transition = "0.4s ease";
+    }
+}
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    var header = document.getElementById("mainHeader");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.style.position = "fixed";
+        header.style.top = "0";
+        header.style.background = "rgba(25, 26, 28, 0.815)";
+        
+    } else {
+        header.style.position = "relative";
+        header.style.background = "rgb(25, 26, 28)";
+    }
+}
