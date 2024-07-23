2b43c5a5fa7e3186a0b3e27e55968fdc7f9f5a59
PNA <pna120104@gmail.com>
2024-07-23 23:04:08 +0700

    Information page

--- KarnelTravelGuideWeb/Controllers/HomeController.cs
+++ KarnelTravelGuideWeb/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace KarnelTravelGuideWeb.Controllers
             return View();
         }
 
+        public IActionResult Information()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
