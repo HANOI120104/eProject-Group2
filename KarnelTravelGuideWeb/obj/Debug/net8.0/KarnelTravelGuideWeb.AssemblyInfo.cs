2341758b6c2ef8c8a6c16b0ea46c83a83a586d56
PNA <pna120104@gmail.com>
2024-07-25 01:33:48 +0700

    layout branch

--- KarnelTravelGuideWeb/obj/Debug/net8.0/KarnelTravelGuideWeb.AssemblyInfo.cs
+++ KarnelTravelGuideWeb/obj/Debug/net8.0/KarnelTravelGuideWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KarnelTravelGuideWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90981a61a108ae6b41722f245f2d6ee7e8bda6e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1dc8d5246f0cceb78d94527c7deb0ac09f3202")]
 [assembly: System.Reflection.AssemblyProductAttribute("KarnelTravelGuideWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KarnelTravelGuideWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
